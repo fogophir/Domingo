dcbb8ee77a1d26cff39bf6067d0f44aee5ca3345
relsteiner <relsteiner@gmail.com>
2014-08-04 16:43:25 +0300

    Change response structure
    {CaptionText,
    User{
    Id,
    UserName,
    FullName,
    ImageUrl},
    Link,
    CreatedTime,
    Image{
    Url,
    Width,
    Height},
    CommentsCount,
    LikesCount,
    RetweetCount}

--- responsebuilder.js
+++ responsebuilder.js
@@ -27,22 +27,30 @@ function getResponseJSONFromInstagramResponse(strInstagramResponse) {
 		else
 			singleResponseObj.CaptionText = null;
 
+		singleResponseObj.User = {};
 		if (currentItem.user != null) {
-			singleResponseObj.UserName = currentItem.user.username;
-			singleResponseObj.UserFullName = currentItem.user.full_name;
+			singleResponseObj.User.Id = currentItem.user.id;
+			singleResponseObj.User.UserName = currentItem.user.username;
+			singleResponseObj.User.FullName = currentItem.user.full_name;
+			singleResponseObj.User.ImageUrl = currentItem.user.profile_picture;
 		} else {
-			singleResponseObj.User = null;
-			singleResponseObj.UserFullName = null;
+			singleResponseObj.User.Id = null;
+			singleResponseObj.UserName = null;
+			singleResponseObj.FullName = null;
+			singleResponseObj.User.ImageUrl = null;
 		}
 
 		singleResponseObj.Link = currentItem.link;
 		singleResponseObj.CreatedTime = currentItem.created_time; // FORMAT!
 
-		singleResponseObj.ThumbImage = currentItem.images.thumbnail.url;
+		singleResponseObj.Image = {};
+		singleResponseObj.Image.Url = currentItem.images.thumbnail.url;
+		singleResponseObj.Image.Width = currentItem.images.thumbnail.width;
+		singleResponseObj.Image.Height = currentItem.images.thumbnail.height;
 
 		singleResponseObj.CommentsCount = currentItem.comments.count;
 		singleResponseObj.LikesCount = currentItem.likes.count;
-		singleResponseObj.retweet = null;
+		singleResponseObj.RetweetCount = null;
 
 		// adding to final response
 		finalResponse.push(singleResponseObj);
@@ -80,22 +88,36 @@ function getResponseJSONFromTwitterResponse(strTwitterResponse) {
 
 		singleResponseObj.CaptionText = tweet.text;
 
+		singleResponseObj.User = {};
 		if (tweet.user != null) {
-			singleResponseObj.UserName = tweet.user.id_str;
-			singleResponseObj.UserFullName = tweet.user.name;
+			singleResponseObj.User.Id = tweet.user.id_str;
+			singleResponseObj.User.UserName = tweet.user.screen_name;
+			singleResponseObj.User.FullName = tweet.user.name;
+			singleResponseObj.User.ImageUrl = tweet.user.profile_image_url;
 		} else {
-			singleResponseObj.User = null;
-			singleResponseObj.UserFullName = null;
+			singleResponseObj.User.Id = null;
+			singleResponseObj.UserName = null;
+			singleResponseObj.FullName = null;
+			singleResponseObj.User.ImageUrl = null;
 		}
 
 		singleResponseObj.Link = 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str;
 		singleResponseObj.CreatedTime = tweet.created_at; // FORMAT!
 
-		singleResponseObj.ThumbImage = null;
+		singleResponseObj.Image = {};
+		if (tweet.entities.hasOwnProperty('media')) {
+			singleResponseObj.Image.Url = tweet.entities.media[0].media_url;
+			singleResponseObj.Image.Width = tweet.entities.media[0].sizes.thumb.w;
+			singleResponseObj.Image.Height = tweet.entities.media[0].sizes.thumb.h;
+		} else {
+			singleResponseObj.Image.Url = null;
+			singleResponseObj.Image.Width = null;
+			singleResponseObj.Image.Height = null;
+		}
 
 		singleResponseObj.CommentsCount = null;
 		singleResponseObj.LikesCount = tweet.favorite_count;
-		singleResponseObj.retweet = tweet.retweet_count;
+		singleResponseObj.RetweetCount = tweet.retweet_count;
 
 		// adding to final response
 		finalResponse.push(singleResponseObj);
